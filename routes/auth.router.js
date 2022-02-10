c8b5a453f5e5bf2d04a01b5f807d772a0a25f2c5
Sumna Islam <sumanislam1416@gmail.com>
2022-02-11 01:27:18 +0600

    add user form credential validation

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 
 const authRouter = express.Router();
 
-authRouter.post('/signup', (req, res) => {
+authRouter.post('/signup',[ 
+  check('email', 'please provide an valid email').isEmail(),
+  check('password', 'password must be at least 7 character long').isLength({ min: 7 })
+], (req, res) => {
   const { email, password } = req.body;
   console.log(email, password);
+
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    })
+  }
 
   res.send('login router working');
 })
